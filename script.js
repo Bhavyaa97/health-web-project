fe5dcbc06af17a375c16e90aa4ad754bc5a51fb1
Bhavyaa97 <shettybvya1997@gmail.com>
2024-07-22 17:40:14 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,655 @@
+const products = [
+  {
+    "respId": "RES00001",
+    "userType": "3",
+    "master": {
+      "offer": null,
+      "pop": null,
+      "profile": null,
+      "tests": [
+        {
+          "name": "FASTINGBLOODSUGAR",
+          "code": "FBS",
+          "aliasName": "",
+          "type": "TEST",
+          "childs": [],
+          "rate": {
+            "b2B": "15",
+            "b2C": "149",
+            "offerRate": "149",
+            "id": "11313",
+            "payAmt": "149",
+            "payAmt1": "149"
+          },
+          "testCount": "1",
+          "benMin": "1",
+          "benMultiple": "1",
+          "benMax": "10",
+          "payType": null,
+          "serum": "FLUORIDE",
+          "edta": "FLUORIDE",
+          "urine": "FLUORIDE",
+          "fluoride": "FLUORIDE",
+          "fasting": "CF",
+          "new": "",
+          "diseaseGroup": "DIABETES",
+          "units": "mg/dL",
+          "volume": null,
+          "normalVal": null,
+          "groupName": "DIABETES",
+          "margin": "35",
+          "hc": null,
+          "specimenType": "FLUORIDE",
+          "testNames": "FASTINGBLOODSUGAR",
+          "additionalTests": null,
+          "validTo": null,
+          "hcrInclude": 0,
+          "ownPkg": "N",
+          "bookedCount": "1026",
+          "barcodes": null,
+          "category": "DIABETES"
+        }
+      ]
+    },
+    "b2BMaster": null,
+    "response": "Success"
+  },
+  {
+    "respId": "RES00001",
+    "userType": "3",
+    "master": {
+      "offer": null,
+      "pop": null,
+      "profile": null,
+      "tests": [
+        {
+          "name": "FASTINGBLOODSUGAR123",
+          "code": "FBS",
+          "aliasName": "",
+          "type": "TEST",
+          "childs": [],
+          "rate": {
+            "b2B": "15",
+            "b2C": "149",
+            "offerRate": "149",
+            "id": "11313",
+            "payAmt": "149",
+            "payAmt1": "149"
+          },
+          "testCount": "1",
+          "benMin": "1",
+          "benMultiple": "1",
+          "benMax": "10",
+          "payType": null,
+          "serum": "FLUORIDE",
+          "edta": "FLUORIDE",
+          "urine": "FLUORIDE",
+          "fluoride": "FLUORIDE",
+          "fasting": "CF",
+          "new": "",
+          "diseaseGroup": "DIABETES",
+          "units": "mg/dL",
+          "volume": null,
+          "normalVal": null,
+          "groupName": "DIABETES",
+          "margin": "35",
+          "hc": null,
+          "specimenType": "FLUORIDE",
+          "testNames": "FASTINGBLOODSUGAR",
+          "additionalTests": null,
+          "validTo": null,
+          "hcrInclude": 0,
+          "ownPkg": "N",
+          "bookedCount": "1026",
+          "barcodes": null,
+          "category": "DIABETES"
+        }
+      ]
+    },
+    "b2BMaster": null,
+    "response": "Success"
+  },
+  {
+    "respId": "RES00001",
+    "userType": "3",
+    "master": {
+      "offer": null,
+      "pop": null,
+      "profile": null,
+      "tests": [
+        {
+          "name": "FASTINGBLOODSUGAR456",
+          "code": "FBS",
+          "aliasName": "",
+          "type": "TEST",
+          "childs": [],
+          "rate": {
+            "b2B": "151",
+            "b2C": "1490",
+            "offerRate": "149",
+            "id": "11313",
+            "payAmt": "149",
+            "payAmt1": "149"
+          },
+          "testCount": "1",
+          "benMin": "1",
+          "benMultiple": "1",
+          "benMax": "10",
+          "payType": null,
+          "serum": "FLUORIDE",
+          "edta": "FLUORIDE",
+          "urine": "FLUORIDE",
+          "fluoride": "FLUORIDE",
+          "fasting": "CF",
+          "new": "",
+          "diseaseGroup": "DIABETES",
+          "units": "mg/dL",
+          "volume": null,
+          "normalVal": null,
+          "groupName": "DIABETES",
+          "margin": "35",
+          "hc": null,
+          "specimenType": "FLUORIDE",
+          "testNames": "FASTINGBLOODSUGAR",
+          "additionalTests": null,
+          "validTo": null,
+          "hcrInclude": 0,
+          "ownPkg": "N",
+          "bookedCount": "1026",
+          "barcodes": null,
+          "category": "DIABETES"
+        }
+      ]
+    },
+    "b2BMaster": null,
+    "response": "Success"
+  },
+  {
+    "respId": "RES00001",
+    "userType": "3",
+    "master": {
+      "offer": null,
+      "pop": null,
+      "profile": null,
+      "tests": [
+        {
+          "name": "FASTINGBLOODSUGAR456",
+          "code": "FBS",
+          "aliasName": "",
+          "type": "TEST",
+          "childs": [],
+          "rate": {
+            "b2B": "151",
+            "b2C": "1490",
+            "offerRate": "149",
+            "id": "11313",
+            "payAmt": "149",
+            "payAmt1": "149"
+          },
+          "testCount": "1",
+          "benMin": "1",
+          "benMultiple": "1",
+          "benMax": "10",
+          "payType": null,
+          "serum": "FLUORIDE",
+          "edta": "FLUORIDE",
+          "urine": "FLUORIDE",
+          "fluoride": "FLUORIDE",
+          "fasting": "CF",
+          "new": "",
+          "diseaseGroup": "DIABETES",
+          "units": "mg/dL",
+          "volume": null,
+          "normalVal": null,
+          "groupName": "DIABETES",
+          "margin": "35",
+          "hc": null,
+          "specimenType": "FLUORIDE",
+          "testNames": "FASTINGBLOODSUGAR",
+          "additionalTests": null,
+          "validTo": null,
+          "hcrInclude": 0,
+          "ownPkg": "N",
+          "bookedCount": "1026",
+          "barcodes": null,
+          "category": "DIABETES"
+        }
+      ]
+    },
+    "b2BMaster": null,
+    "response": "Success"
+  },
+  {
+    "respId": "RES00001",
+    "userType": "3",
+    "master": {
+      "offer": null,
+      "pop": null,
+      "profile": null,
+      "tests": [
+        {
+          "name": "FASTINGBLOODSUGAR456",
+          "code": "FBS",
+          "aliasName": "",
+          "type": "TEST",
+          "childs": [],
+          "rate": {
+            "b2B": "151",
+            "b2C": "1490",
+            "offerRate": "149",
+            "id": "11313",
+            "payAmt": "149",
+            "payAmt1": "149"
+          },
+          "testCount": "1",
+          "benMin": "1",
+          "benMultiple": "1",
+          "benMax": "10",
+          "payType": null,
+          "serum": "FLUORIDE",
+          "edta": "FLUORIDE",
+          "urine": "FLUORIDE",
+          "fluoride": "FLUORIDE",
+          "fasting": "CF",
+          "new": "",
+          "diseaseGroup": "DIABETES",
+          "units": "mg/dL",
+          "volume": null,
+          "normalVal": null,
+          "groupName": "DIABETES",
+          "margin": "35",
+          "hc": null,
+          "specimenType": "FLUORIDE",
+          "testNames": "FASTINGBLOODSUGAR",
+          "additionalTests": null,
+          "validTo": null,
+          "hcrInclude": 0,
+          "ownPkg": "N",
+          "bookedCount": "1026",
+          "barcodes": null,
+          "category": "DIABETES"
+        }
+      ]
+    },
+    "b2BMaster": null,
+    "response": "Success"
+  },
+  {
+    "respId": "RES00001",
+    "userType": "3",
+    "master": {
+      "offer": null,
+      "pop": null,
+      "profile": null,
+      "tests": [
+        {
+          "name": "FASTINGBLOODSUGAR456",
+          "code": "FBS",
+          "aliasName": "",
+          "type": "TEST",
+          "childs": [],
+          "rate": {
+            "b2B": "151",
+            "b2C": "1490",
+            "offerRate": "149",
+            "id": "11313",
+            "payAmt": "149",
+            "payAmt1": "149"
+          },
+          "testCount": "1",
+          "benMin": "1",
+          "benMultiple": "1",
+          "benMax": "10",
+          "payType": null,
+          "serum": "FLUORIDE",
+          "edta": "FLUORIDE",
+          "urine": "FLUORIDE",
+          "fluoride": "FLUORIDE",
+          "fasting": "CF",
+          "new": "",
+          "diseaseGroup": "DIABETES",
+          "units": "mg/dL",
+          "volume": null,
+          "normalVal": null,
+          "groupName": "DIABETES",
+          "margin": "35",
+          "hc": null,
+          "specimenType": "FLUORIDE",
+          "testNames": "FASTINGBLOODSUGAR",
+          "additionalTests": null,
+          "validTo": null,
+          "hcrInclude": 0,
+          "ownPkg": "N",
+          "bookedCount": "1026",
+          "barcodes": null,
+          "category": "DIABETES"
+        }
+      ]
+    },
+    "b2BMaster": null,
+    "response": "Success"
+  },
+  {
+    "respId": "RES00001",
+    "userType": "3",
+    "master": {
+      "offer": null,
+      "pop": null,
+      "profile": null,
+      "tests": [
+        {
+          "name": "FASTINGBLOODSUGAR456",
+          "code": "FBS",
+          "aliasName": "",
+          "type": "TEST",
+          "childs": [],
+          "rate": {
+            "b2B": "151",
+            "b2C": "1490",
+            "offerRate": "149",
+            "id": "11313",
+            "payAmt": "149",
+            "payAmt1": "149"
+          },
+          "testCount": "1",
+          "benMin": "1",
+          "benMultiple": "1",
+          "benMax": "10",
+          "payType": null,
+          "serum": "FLUORIDE",
+          "edta": "FLUORIDE",
+          "urine": "FLUORIDE",
+          "fluoride": "FLUORIDE",
+          "fasting": "CF",
+          "new": "",
+          "diseaseGroup": "DIABETES",
+          "units": "mg/dL",
+          "volume": null,
+          "normalVal": null,
+          "groupName": "DIABETES",
+          "margin": "35",
+          "hc": null,
+          "specimenType": "FLUORIDE",
+          "testNames": "FASTINGBLOODSUGAR",
+          "additionalTests": null,
+          "validTo": null,
+          "hcrInclude": 0,
+          "ownPkg": "N",
+          "bookedCount": "1026",
+          "barcodes": null,
+          "category": "DIABETES"
+        }
+      ]
+    },
+    "b2BMaster": null,
+    "response": "Success"
+  },
+  {
+    "respId": "RES00001",
+    "userType": "3",
+    "master": {
+      "offer": null,
+      "pop": null,
+      "profile": null,
+      "tests": [
+        {
+          "name": "FASTINGBLOODSUGAR456",
+          "code": "FBS",
+          "aliasName": "",
+          "type": "TEST",
+          "childs": [],
+          "rate": {
+            "b2B": "151",
+            "b2C": "1490",
+            "offerRate": "149",
+            "id": "11313",
+            "payAmt": "149",
+            "payAmt1": "149"
+          },
+          "testCount": "1",
+          "benMin": "1",
+          "benMultiple": "1",
+          "benMax": "10",
+          "payType": null,
+          "serum": "FLUORIDE",
+          "edta": "FLUORIDE",
+          "urine": "FLUORIDE",
+          "fluoride": "FLUORIDE",
+          "fasting": "CF",
+          "new": "",
+          "diseaseGroup": "DIABETES",
+          "units": "mg/dL",
+          "volume": null,
+          "normalVal": null,
+          "groupName": "DIABETES",
+          "margin": "35",
+          "hc": null,
+          "specimenType": "FLUORIDE",
+          "testNames": "FASTINGBLOODSUGAR",
+          "additionalTests": null,
+          "validTo": null,
+          "hcrInclude": 0,
+          "ownPkg": "N",
+          "bookedCount": "1026",
+          "barcodes": null,
+          "category": "DIABETES"
+        }
+      ]
+    },
+    "b2BMaster": null,
+    "response": "Success"
+  },
+  {
+    "respId": "RES00001",
+    "userType": "3",
+    "master": {
+      "offer": null,
+      "pop": null,
+      "profile": null,
+      "tests": [
+        {
+          "name": "FASTINGBLOODSUGAR456",
+          "code": "FBS",
+          "aliasName": "",
+          "type": "TEST",
+          "childs": [],
+          "rate": {
+            "b2B": "151",
+            "b2C": "1490",
+            "offerRate": "149",
+            "id": "11313",
+            "payAmt": "149",
+            "payAmt1": "149"
+          },
+          "testCount": "1",
+          "benMin": "1",
+          "benMultiple": "1",
+          "benMax": "10",
+          "payType": null,
+          "serum": "FLUORIDE",
+          "edta": "FLUORIDE",
+          "urine": "FLUORIDE",
+          "fluoride": "FLUORIDE",
+          "fasting": "CF",
+          "new": "",
+          "diseaseGroup": "DIABETES",
+          "units": "mg/dL",
+          "volume": null,
+          "normalVal": null,
+          "groupName": "DIABETES",
+          "margin": "35",
+          "hc": null,
+          "specimenType": "FLUORIDE",
+          "testNames": "FASTINGBLOODSUGAR",
+          "additionalTests": null,
+          "validTo": null,
+          "hcrInclude": 0,
+          "ownPkg": "N",
+          "bookedCount": "1026",
+          "barcodes": null,
+          "category": "DIABETES"
+        }
+      ]
+    },
+    "b2BMaster": null,
+    "response": "Success"
+  },
+  {
+    "respId": "RES00001",
+    "userType": "3",
+    "master": {
+      "offer": null,
+      "pop": null,
+      "profile": null,
+      "tests": [
+        {
+          "name": "FASTINGBLOODSUGAR456",
+          "code": "FBS",
+          "aliasName": "",
+          "type": "TEST",
+          "childs": [],
+          "rate": {
+            "b2B": "151",
+            "b2C": "1490",
+            "offerRate": "149",
+            "id": "11313",
+            "payAmt": "149",
+            "payAmt1": "149"
+          },
+          "testCount": "1",
+          "benMin": "1",
+          "benMultiple": "1",
+          "benMax": "10",
+          "payType": null,
+          "serum": "FLUORIDE",
+          "edta": "FLUORIDE",
+          "urine": "FLUORIDE",
+          "fluoride": "FLUORIDE",
+          "fasting": "CF",
+          "new": "",
+          "diseaseGroup": "DIABETES",
+          "units": "mg/dL",
+          "volume": null,
+          "normalVal": null,
+          "groupName": "DIABETES",
+          "margin": "35",
+          "hc": null,
+          "specimenType": "FLUORIDE",
+          "testNames": "FASTINGBLOODSUGAR",
+          "additionalTests": null,
+          "validTo": null,
+          "hcrInclude": 0,
+          "ownPkg": "N",
+          "bookedCount": "1026",
+          "barcodes": null,
+          "category": "DIABETES"
+        }
+      ]
+    },
+    "b2BMaster": null,
+    "response": "Success"
+  },
+  {
+    "respId": "RES00001",
+    "userType": "3",
+    "master": {
+      "offer": null,
+      "pop": null,
+      "profile": null,
+      "tests": [
+        {
+          "name": "FASTINGBLOODSUGAR456",
+          "code": "FBS",
+          "aliasName": "",
+          "type": "TEST",
+          "childs": [],
+          "rate": {
+            "b2B": "151",
+            "b2C": "1490",
+            "offerRate": "149",
+            "id": "11313",
+            "payAmt": "149",
+            "payAmt1": "149"
+          },
+          "testCount": "1",
+          "benMin": "1",
+          "benMultiple": "1",
+          "benMax": "10",
+          "payType": null,
+          "serum": "FLUORIDE",
+          "edta": "FLUORIDE",
+          "urine": "FLUORIDE",
+          "fluoride": "FLUORIDE",
+          "fasting": "CF",
+          "new": "",
+          "diseaseGroup": "DIABETES",
+          "units": "mg/dL",
+          "volume": null,
+          "normalVal": null,
+          "groupName": "DIABETES",
+          "margin": "35",
+          "hc": null,
+          "specimenType": "FLUORIDE",
+          "testNames": "FASTINGBLOODSUGAR",
+          "additionalTests": null,
+          "validTo": null,
+          "hcrInclude": 0,
+          "ownPkg": "N",
+          "bookedCount": "1026",
+          "barcodes": null,
+          "category": "DIABETES"
+        }
+      ]
+    },
+    "b2BMaster": null,
+    "response": "Success"
+  }
+];
+
+function createProductCard(product) {
+  const test = product.master.tests[0];
+  const card = document.createElement('div');
+  card.className = 'product-apiwork';
+
+  card.innerHTML = `
+    <img src="./medical-doctor.webp" alt="${test.name}">
+    <h3>${test.name}</h3>
+    <p class="price-apiwork">
+      <span class="original-price-apiwork">${test.rate.b2B}</span>
+      <span class="discounted-price-apiwork">${test.rate.b2C}</span>
+    </p>
+    <a href="https://prognohealth.com/product/healthy-life-advance-female-299searchhealthylifeadvancefemaleattr212223/">
+      <button class="view-apiwork">View</button>
+    </a>
+    <button class="book-apiwork">Book Now</button>
+  `;
+
+  return card;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const mainContent = document.querySelector('.main-content-apiwork');
+  products.forEach(product => {
+    mainContent.appendChild(createProductCard(product));
+  });
+});
+
+const searchInput = document.getElementById('searchInput-apiwork');
+const suggestionsContainer = document.getElementById('suggestions-apiwork');
+
+searchInput.addEventListener('input', function () {
+  const searchText = this.value.toLowerCase().trim();
+  const filteredProducts = products.filter(product =>
+    product.master.tests[0].name.toLowerCase().includes(searchText)
+  );
+
+  suggestionsContainer.innerHTML = '';
+
+  filteredProducts.forEach(product => {
+    const suggestion = document.createElement('li');
+    suggestion.textContent = product.master.tests[0].name;
+    suggestionsContainer.appendChild(suggestion);
+  });
+
+  suggestionsContainer.style.display = filteredProducts.length > 0 ? 'block' : 'none';
+});
